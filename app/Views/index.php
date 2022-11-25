9b95fd428f3b2ca4fae60f0eb59b7e6e5657a7c8
liekenfta <liekentri@gmail.com>
2022-11-25 13:02:56 +0700

    update
    update

---
+++ app/Views/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta rel="stylesheet" href="<base_url('assets/css/bootstrap.min.css');
+    <title>Home</title>
+</head>
+<body>
+    <script src="<?= base_url('assets/js/bootstrap.min.js'); ?>"></script>"
+</body>
+
+</html>
